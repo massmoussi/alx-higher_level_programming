00fc42cb16ec63400859a01880dc5b3eaaf2ad50
killua <killua@killua2.0>
2024-03-18 19:13:42 +0000

    dddddd

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,44 +9,44 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = *head;
-    int size = 0, i;
-    int *arr;
+	listint_t *current = *head;
+	int size = 0, i;
+	int *arr;
 
-    if (head == NULL || *head == NULL)
-        return (1);
+	if (head == NULL || *head == NULL)
+		return (1);
 
-    /* Count the number of nodes in the list */
-    while (current)
-    {
-        size++;
-        current = current->next;
-    }
+	/* Count the number of nodes in the list */
+	while (current)
+	{
+		size++;
+		current = current->next;
+	}
 
-    /* Allocate memory for the array */
-    arr = malloc(sizeof(int) * size);
-    if (arr == NULL)
-        return (0);
+	/* Allocate memory for the array */
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (0);
 
-    /* Store the values of the list in the array */
-    current = *head;
-    for (i = 0; i < size; i++)
-    {
-        arr[i] = current->n;
-        current = current->next;
-    }
+	/* Store the values of the list in the array */
+	current = *head;
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = current->n;
+		current = current->next;
+	}
 
-    /* Check if the array is a palindrome */
-    for (i = 0; i < size / 2; i++)
-    {
-        if (arr[i] != arr[size - i - 1])
-        {
-            free(arr);
-            return (0);
-        }
-    }
+	/* Check if the array is a palindrome */
+	for (i = 0; i < size / 2; i++)
+	{
+		if (arr[i] != arr[size - i - 1])
+		{
+			free(arr);
+			return (0);
+		}
+	}
 
-    free(arr);
-    return (1);
+	free(arr);
+	return (1);
 }
 
