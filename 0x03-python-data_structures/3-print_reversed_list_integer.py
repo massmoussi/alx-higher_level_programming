5a9a5e247814b627f51bf3efddceb3e4258615c5
killua <killua@killua2.0>
2024-03-18 18:31:55 +0000

    dd

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{}".format(num))
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{}".format(my_list[i]))
