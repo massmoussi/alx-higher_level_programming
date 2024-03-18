0405e0afd79835886ac2243e0174886830348e3a
killua <killua@killua2.0>
2024-03-18 16:01:12 +0000

    unusel

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    ctoremove = "cC"
-    new_string = ''.join(char for char in my_string if char != ctoremove)
+    ctoremove = "c"
+    ctomove = "C"
+    new_string = ''.join(char for char in my_string
+                         if char != ctoremove and char != ctomove)
     return new_string
