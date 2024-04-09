8bd15df8d838904a7cce80380afc7517a7ea5ad8
killua <killua@killua2.0>
2024-04-09 23:55:19 +0000

    kk

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Locked Class"""
+
+
+class LockedClass:
+    """
+    This is Locked Class accept only a certain attribute name
+    __slots__ usage
+    """
+    __slots__ = ["first_name"]
