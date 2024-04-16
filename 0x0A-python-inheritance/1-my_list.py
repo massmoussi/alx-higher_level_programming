57f177ca0109178d67b6a332f5a7f9ec2df891bf
massmoussi <assermouh.abdelhak2019@gmail.com>
2024-04-16 01:31:30 +0100

    deee

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,8 +8,6 @@ class MyList(list):
     sort a list of int using sorted function
     also via inheretance Concept
     """
-    def __init__(self, *args):
-        super().__init__(self, *args)
 
     def print_sorted(self):
         sort_list = sorted(self)
