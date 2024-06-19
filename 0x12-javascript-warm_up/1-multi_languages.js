d11d85f9dc7f4a2f563a3f764ef40f6940803ba4
killua <killua@killua2.0>
2024-06-19 01:56:23 +0100

    pp

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 
-console.log(`
-C is fun
-Python is cool
-JavaScript is amazing
-`);
+const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const element of content) {
+  console.log(element);
+}
