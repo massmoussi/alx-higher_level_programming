f7f9a94c4bf65d14d4845a8407c9a852307135a4
killua <killua@killua2.0>
2024-06-23 14:55:12 +0100

    dd

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Lst all data stats from stt Tbl order by id.
+(args) a gotten from sys.argv without check
+"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db_conn = MySQLdb.connect(user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3],
+                              host='localhost',
+                              port=3306)
+
+    cur = db_conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db_conn.close()
