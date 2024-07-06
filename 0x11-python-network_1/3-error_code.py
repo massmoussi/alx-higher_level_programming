18d81b31510ab02d2a9de8e30fdc699ad706501e
killua <killua@killua2.0>
2024-07-06 01:04:45 +0100

    llp

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    Handl diff Status Code with Try/expect
+        if 200 it return utf-8 decoded response
+        if Error appear expect handl it & retrn the code
+"""
+
 
 if __name__ == '__main__':
     import urllib.request
