95f0e7242932326e77452739141c855619d0d3cc
killua <killua@killua2.0>
2024-07-08 13:00:22 +0100

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This script fetches https://alx-intranet.hbtn.io/status
+and displays the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
