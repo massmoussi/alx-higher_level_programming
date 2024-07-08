34c3291f5659243419d98724ef0f944ab2d0fe0e
killua <killua@killua2.0>
2024-07-08 13:02:16 +0100

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
-    
+
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
     print(f"\t- content: {response.text}")
